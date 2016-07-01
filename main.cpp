2bfe7470668282fade94390885183bd424afe7f4
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2016-07-01 22:36:21 +0300

    saving pid to file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
 using namespace std;
 
 void *thread_func(void *arg)
 {
   int *val = (int *) arg;
   for(int i = 0; i < 10; i++)
+  {
+    usleep(3000000);
     (*val)++;
+    cout << "wait ... " << i << endl;
+  }
   return val;
 }
 
@@ -19,6 +25,13 @@ int main(int argc, char *argv[])
   {
     cerr << "thread create fail" << endl;
     return -1;
+  }
+  pid_t pid = getpid();
+  FILE *fp = fopen("/home/box/main.pid", "w");
+  if(fp != NULL)
+  {
+    fprintf(fp, "%d", (int) pid);
+    fclose(fp);
   }
   int *ret = NULL;
   pthread_join(thread_id, (void **) &ret);
